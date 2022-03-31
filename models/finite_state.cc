7844e3e0e67871fa26af27d9b71692976ef15356
Hackjaku <stressfest@rocketmail.com>
2022-03-31 18:04:11 +0200

    UPDATE: classes

---
+++ models/finite_state.cc
@@ -0,0 +1,20 @@
+#ifndef FINITE_STATE
+#define FINITE_STATE
+
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/io.hpp>
+#include <iostream>
+#include <vector>
+#include <string>
+#include "../utils/vector_utils.h"
+
+namespace machines {
+    using namespace std;
+    class FiniteState {
+    public:
+        FiniteState();
+    private:
+    };
+}
+
+#endif
